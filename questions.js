d508afcae4e6d67c6b53dba84f796ea72d805598
TaraWeidner <taratalnenetz@gmail.com>
2026-07-30 09:54:30 -0700

    Add prototype question data

---
+++ questions.js
@@ -0,0 +1,53 @@
+const categories = [
+  { name: 'Slashers', questions: [
+    ['Which masked killer stalks Haddonfield?', ['Michael Myers','Jason Voorhees','Ghostface','Leatherface'], 'Michael Myers', 'The Shape first appeared in John Carpenter’s Halloween.'],
+    ['In the first Friday the 13th film, who is the killer?', ['Jason Voorhees','Pamela Voorhees','Tommy Jarvis','Roy Burns'], 'Pamela Voorhees', 'Jason becomes the central killer later in the series.'],
+    ['Which weapon is most associated with Leatherface?', ['Machete','Chainsaw','Hook','Axe'], 'Chainsaw', 'Leatherface is the chainsaw-wielding figure from The Texas Chain Saw Massacre.'],
+    ['Which film introduced Ghostface?', ['Scream','Urban Legend','The Strangers','Candyman'], 'Scream', 'Ghostface debuted in Wes Craven’s 1996 film.'],
+    ['What is fitted to Freddy Krueger’s glove?', ['Needles','Razors','Hooks','Scissors'], 'Razors', 'Freddy’s glove carries four razor blades.']
+  ]},
+  { name: 'TV Terror', questions: [
+    ['Which family lives at 1313 Mockingbird Lane?', ['The Addamses','The Munsters','The Crains','The Spellmans'], 'The Munsters', 'The Munsters live at 1313 Mockingbird Lane.'],
+    ['What town is the setting of Stranger Things?', ['Sunnydale','Hawkins','Derry','Mystic Falls'], 'Hawkins', 'The series is set in fictional Hawkins, Indiana.'],
+    ['Which series features Murder House, Asylum, and Coven?', ['Supernatural','American Horror Story','The Haunting','Channel Zero'], 'American Horror Story', 'Those are seasons of the anthology series.'],
+    ['What is Sunnydale built over in Buffy?', ['A cemetery','A Hellmouth','An ancient crypt','A portal to Oz'], 'A Hellmouth', 'Sunnydale sits directly over a Hellmouth.'],
+    ['Which series follows agents Mulder and Scully?', ['Fringe','The X-Files','Evil','Twin Peaks'], 'The X-Files', 'They investigate paranormal cases known as X-Files.']
+  ]},
+  { name: 'Haunted Lore', questions: [
+    ['Which spirit’s wail is said to foretell death?', ['Banshee','Kelpie','Brownie','Selkie'], 'Banshee', 'The banshee comes from Irish folklore.'],
+    ['The Winchester Mystery House is in which state?', ['Oregon','California','Nevada','Massachusetts'], 'California', 'It stands in San Jose, California.'],
+    ['Which creature is tied to Point Pleasant, West Virginia?', ['Jersey Devil','Mothman','Flatwoods Monster','Fresno Nightcrawler'], 'Mothman', 'Mothman sightings became linked to Point Pleasant in the 1960s.'],
+    ['Which ghost ship is doomed to sail forever?', ['Flying Dutchman','Mary Celeste','Queen Anne','Black Swan'], 'Flying Dutchman', 'The Flying Dutchman is said to be unable to make port.'],
+    ['Poltergeist stories usually involve what?', ['Dreams','Moving objects and noises','Weather changes','Shape-shifting'], 'Moving objects and noises', 'Knocks and thrown objects are classic poltergeist activity.']
+  ]},
+  { name: 'Halloween', questions: [
+    ['Which ancient festival is often linked to Halloween?', ['Beltane','Samhain','Lupercalia','Saturnalia'], 'Samhain', 'Samhain marked the end of harvest and beginning of winter.'],
+    ['Before pumpkins, what was commonly carved in Ireland?', ['Turnips','Potatoes','Beets','Cabbages'], 'Turnips', 'Turnips and other root vegetables were carved into lanterns.'],
+    ['Which candy was once marketed as Chicken Feed?', ['Candy corn','Peeps','Tootsie Rolls','Bit-O-Honey'], 'Candy corn', 'Candy corn once used the name Chicken Feed.'],
+    ['What color is the classic plastic pumpkin candy bucket?', ['Orange','Green','Purple','Blue'], 'Orange', 'The orange jack-o’-lantern bucket became a Halloween staple.'],
+    ['Which line follows “Trick or treat” in the familiar rhyme?', ['Smell my feet','Give me something good to eat','Both','Neither'], 'Both', 'The playground rhyme commonly uses both lines.']
+  ]},
+  { name: 'Nightmare ID', questions: [
+    ['A videotape, a phone call, and seven days point to which film?', ['The Ring','Sinister','Pulse','Shutter'], 'The Ring', 'The cursed tape brings a seven-day death sentence.'],
+    ['A puzzle box summons which beings?', ['Cenobites','Deadites','The Strangers','The Gentlemen'], 'Cenobites', 'The Lament Configuration summons the Cenobites in Hellraiser.'],
+    ['Which hotel is central to The Shining?', ['The Stanley','The Overlook','The Cortez','The Bates'], 'The Overlook', 'The Torrance family winters at the Overlook Hotel.'],
+    ['Jaws terrorizes which fictional island?', ['Skull Island','Amity Island','Summerisle','Shutter Island'], 'Amity Island', 'Jaws takes place around fictional Amity Island.'],
+    ['Which film includes the line “They’re here”?', ['Poltergeist','The Exorcist','The Omen','The Others'], 'Poltergeist', 'Carol Anne announces the arrival with the famous line.']
+  ]},
+  { name: 'Deep Dark', questions: [
+    ['Who wrote The Colour Out of Space?', ['Edgar Allan Poe','H. P. Lovecraft','Algernon Blackwood','Arthur Machen'], 'H. P. Lovecraft', 'Lovecraft published the story in 1927.'],
+    ['Which 1922 film introduced Count Orlok?', ['Häxan','Nosferatu','The Golem','Dr. Mabuse'], 'Nosferatu', 'Count Orlok is the vampire in F. W. Murnau’s film.'],
+    ['What is the demon’s name in The Exorcist?', ['Pazuzu','Paimon','Bagul','Valak'], 'Pazuzu', 'Pazuzu is associated with Regan’s possession.'],
+    ['Who created the haunted Hill House?', ['Shirley Jackson','Daphne du Maurier','Anne Rice','Susan Hill'], 'Shirley Jackson', 'Jackson wrote The Haunting of Hill House.'],
+    ['Which 1960 film was adapted from Robert Bloch’s novel?', ['Peeping Tom','Psycho','Eyes Without a Face','Black Sunday'], 'Psycho', 'Hitchcock’s film was adapted from Bloch’s 1959 novel.']
+  ]}
+];
+
+const bonus = {
+  category: 'Dungeon Crawler Carl',
+  value: 1000,
+  text: 'What is the name of Princess Donut’s dinosaur companion?',
+  choices: ['Mongo','Gravy Boat','Ferdinand','Kiwi'],
+  answer: 'Mongo',
+  fact: 'Mongo is appalled that anyone might miss this question.'
+};
