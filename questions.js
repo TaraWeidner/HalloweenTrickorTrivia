d4a32baaeeed51ba928ae094caef57d78de0c3fb
TaraWeidner <taratalnenetz@gmail.com>
2026-07-30 12:02:53 -0700

    Rename prototype categories for game-night theme

--- questions.js
+++ questions.js
@@ -1,5 +1,5 @@
 const categories = [
-  { name: 'Slashers', questions: [
+  { name: 'Slashers & Scream Queens', questions: [
     ['Which masked killer stalks Haddonfield?', ['Michael Myers','Jason Voorhees','Ghostface','Leatherface'], 'Michael Myers', 'The Shape first appeared in John Carpenter’s Halloween.'],
     ['In the first Friday the 13th film, who is the killer?', ['Jason Voorhees','Pamela Voorhees','Tommy Jarvis','Roy Burns'], 'Pamela Voorhees', 'Jason becomes the central killer later in the series.'],
     ['Which weapon is most associated with Leatherface?', ['Machete','Chainsaw','Hook','Axe'], 'Chainsaw', 'Leatherface is the chainsaw-wielding figure from The Texas Chain Saw Massacre.'],
@@ -20,21 +20,21 @@ const categories = [
     ['Which ghost ship is doomed to sail forever?', ['Flying Dutchman','Mary Celeste','Queen Anne','Black Swan'], 'Flying Dutchman', 'The Flying Dutchman is said to be unable to make port.'],
     ['Poltergeist stories usually involve what?', ['Dreams','Moving objects and noises','Weather changes','Shape-shifting'], 'Moving objects and noises', 'Knocks and thrown objects are classic poltergeist activity.']
   ]},
-  { name: 'Halloween', questions: [
+  { name: 'Halloween History', questions: [
     ['Which ancient festival is often linked to Halloween?', ['Beltane','Samhain','Lupercalia','Saturnalia'], 'Samhain', 'Samhain marked the end of harvest and beginning of winter.'],
     ['Before pumpkins, what was commonly carved in Ireland?', ['Turnips','Potatoes','Beets','Cabbages'], 'Turnips', 'Turnips and other root vegetables were carved into lanterns.'],
     ['Which candy was once marketed as Chicken Feed?', ['Candy corn','Peeps','Tootsie Rolls','Bit-O-Honey'], 'Candy corn', 'Candy corn once used the name Chicken Feed.'],
     ['What color is the classic plastic pumpkin candy bucket?', ['Orange','Green','Purple','Blue'], 'Orange', 'The orange jack-o’-lantern bucket became a Halloween staple.'],
     ['Which line follows “Trick or treat” in the familiar rhyme?', ['Smell my feet','Give me something good to eat','Both','Neither'], 'Both', 'The playground rhyme commonly uses both lines.']
   ]},
-  { name: 'Nightmare ID', questions: [
+  { name: 'Name That Nightmare', questions: [
     ['A videotape, a phone call, and seven days point to which film?', ['The Ring','Sinister','Pulse','Shutter'], 'The Ring', 'The cursed tape brings a seven-day death sentence.'],
     ['A puzzle box summons which beings?', ['Cenobites','Deadites','The Strangers','The Gentlemen'], 'Cenobites', 'The Lament Configuration summons the Cenobites in Hellraiser.'],
     ['Which hotel is central to The Shining?', ['The Stanley','The Overlook','The Cortez','The Bates'], 'The Overlook', 'The Torrance family winters at the Overlook Hotel.'],
     ['Jaws terrorizes which fictional island?', ['Skull Island','Amity Island','Summerisle','Shutter Island'], 'Amity Island', 'Jaws takes place around fictional Amity Island.'],
     ['Which film includes the line “They’re here”?', ['Poltergeist','The Exorcist','The Omen','The Others'], 'Poltergeist', 'Carol Anne announces the arrival with the famous line.']
   ]},
-  { name: 'Deep Dark', questions: [
+  { name: 'The Deep Dark', questions: [
     ['Who wrote The Colour Out of Space?', ['Edgar Allan Poe','H. P. Lovecraft','Algernon Blackwood','Arthur Machen'], 'H. P. Lovecraft', 'Lovecraft published the story in 1927.'],
     ['Which 1922 film introduced Count Orlok?', ['Häxan','Nosferatu','The Golem','Dr. Mabuse'], 'Nosferatu', 'Count Orlok is the vampire in F. W. Murnau’s film.'],
     ['What is the demon’s name in The Exorcist?', ['Pazuzu','Paimon','Bagul','Valak'], 'Pazuzu', 'Pazuzu is associated with Regan’s possession.'],
